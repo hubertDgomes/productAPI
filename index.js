b3697c7b56a7029bd17e756b66c293e586314fc0
hubertDgomes <gomeshubert8@gmail.com>
2026-02-24 13:32:32 +0600

    Format index.js: Organize imports and add newlines for improved readability

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const app = express()
 import routes from "./routes/allRoutes.js"
 import productAPI from "./routes/productAPI.js"
 import session from "express-session"
+
 import path from "path"
 import { fileURLToPath } from "url"
+
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -22,10 +24,17 @@ app.use(session({
 app.get("/",(req,res)=>{
     res.send("Server is running!")
 })
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+
+
 dbConfigure()
+
+
 app.use("/api" , routes)
 app.use("/product" , productAPI)
+
+
 app.listen(3000,()=>{
     console.log("Server is running at 3000");
 })
